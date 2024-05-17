a7dd49c782fb94f07c97a69ccbd4a089bcb50066
Your Name <you@example.com>
2024-05-17 15:10:43 +0400

    добавили следующий код

---
+++ data_create.py
@@ -0,0 +1,22 @@
+
+
+def name_data():
+    name = input('Введите ваше имя: ')
+    print('Очень красивое имя!')
+    return name
+
+
+def surname_data():
+    surname = input('Введите ваше фамилию: ')
+    return surname
+
+
+def phone_data():
+    phone = input('Введите ваш телефон: ')
+    return phone
+
+
+
+def address_data():
+    address = input('Введите ваш адресс: ')
+    return address
